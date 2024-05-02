ba9ef494c791816a02f88d716117694b6c0a567e
IsaacCorellaA <143538498+IsaacCorellaA@users.noreply.github.com>
2024-05-01 19:49:15 -0600

    Changes test
    ....test

--- parteConTailwind/tailwind.config.js
+++ parteConTailwind/tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./src/*.html'],
   theme: {
-    extend: {},
+    extend: {
+
+
+      backgroundSign: {
+        "gray-background": "#2c2d2d",
+      },
+
+      colors: {
+        "background-color": "#5677FC",
+        "textColor": "#5677FC",
+      },
+
+
+      mainFont: {
+        "fontFamily": ['Roboto', 'sans-serif'],
+      },
+
+      active: {
+        "color": "#5677FC",
+        "text-decoration": "underline"
+      }
+    }
   },
   plugins: [],
 }
