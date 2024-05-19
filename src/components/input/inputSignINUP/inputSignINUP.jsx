d271d51a92adabd129c1a24183d15fc90493225f
IsaacCorellaA <143538498+IsaacCorellaA@users.noreply.github.com>
2024-05-19 03:21:10 -0600

    Sign Up - completed
    Something is wrong with the fuction that changes the Sign In to Sign Up.

--- src/components/input/inputSignINUP/inputSignINUP.jsx
+++ src/components/input/inputSignINUP/inputSignINUP.jsx
@@ -2,7 +2,7 @@ export default function InputSignINUP({title , placeholder}) {
     return (
         <>
         <h3 class="font-bold text-black pb-2">{title}</h3>
-        <input class="w-[35%] border-b-2 border-black focus:outline-none bg-transparent text-black" placeholder={placeholder}></input>
+        <input class="w-[60%] border-b-2 border-black focus:outline-none bg-transparent text-black" placeholder={placeholder}></input>
         </>
     );
 }
--- src/login.jsx
+++ src/login.jsx
@@ -21,17 +21,17 @@ export default function Login() {
               />
             </div>
 
-            <div className="flex flex-row pl-23 justify-start gap-3 pt-20 pb-24 items-center">
+            <div className="flex flex-row pl-23 md:justify-start gap-3 pt-20 pb-24">
               <a
-                className="text-black text-5xl font-light hover:text-blue-600"
+                className="text-black lg:text-5xl font-light hover:text-blue-600"
                 href=""
                 onClick={showSignUp}
               >
                 Sign Up
               </a>
-              <p className="text-black text-3xl ">or</p>
+              <p className="text-black lg:text-3xl ">or</p>
               <a
-                className="text-black text-5xl font-light hover:text-blue-600"
+                className="text-black lg:text-5xl font-light hover:text-blue-600"
                 href=""
                 onClick={showSignIn}
               >
@@ -56,12 +56,65 @@ export default function Login() {
               </a>
             </div>
           </div>
+
+        </section>
+
+        <section id="sign-up" className="hidden">
+          <div className="grid pl-20 duration-300">
+            <div className="pt-10 duration-300">
+              <img
+                className="w-80 h-auto"
+                src="./src/imgs/Logo_white.png"
+                alt=""
+              />
+            </div>
+
+            <div className="flex flex-row pl-23 md:justify-start gap-3 pt-20 pb-24">
+              <a
+                className="text-black lg:text-5xl font-light hover:text-blue-600"
+                href=""
+                onClick={showSignUp}
+              >
+                Sign Up
+              </a>
+              <p className="text-black lg:text-3xl ">or</p>
+              <a
+                className="text-black lg:text-5xl font-light hover:text-blue-600"
+                href=""
+                onClick={showSignIn}
+              >
+                Sign In
+              </a>
+            </div>
+
+                
+            <div className="gap-5 flex flex-col">
+              <InputLogin title={"FULLNAME"} placeholder={"Enter your fullname"} />
+              <InputLogin
+                title={"PASSWORD"}
+                placeholder={"Enter your password"}
+              />
+              <InputLogin
+                title={"E-MAIL"}
+                placeholder={"Enter your e-mail"}
+              />
+            </div>
+            
+
+            <div class="flex flex-row gap-7 items-center pt-5">
+              <Button variant="solid-discord-blue">Sign In</Button>
+              <a class="text-black hover:underline hover:text-blue-500" href="">
+                Did you forget your password?
+              </a>
+            </div>
+          </div>
+
         </section>
 
 
 
-        <section className="w-[100%]">
-            <img className="h-screen" src="./src/imgs/background_sign.png" alt="background_signIn" />
+        <section className="w-[100%] flex justify-end">
+            <img className="h-screen hidden md:block" src="./src/imgs/background_sign.png" alt="background_signIn" />
         </section>
 
 
