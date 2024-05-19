6915478dee295265d25da7f4468d6d807608a0d7
IsaacCorellaA <143538498+IsaacCorellaA@users.noreply.github.com>
2024-05-19 01:52:34 -0600

    Login React part.2

--- src/login.jsx
+++ src/login.jsx
@@ -1,46 +1,78 @@
-import InputLogin from "./components/input/inputText/inputText.jsx";
+import InputLogin from "./components/input/inputSignINUP/inputSignINUP.jsx";
+import { showSignUp, showSignIn, showSignInFromSignUp } from "./utils.js";
+import Button from "./components/button/button.jsx";
 
 export default function Login() {
-    return (
-        <>
-            <body className="bg-white">
-                <section id="sign-in" className="visible" >
-                    <div className="grid grid-cols-2 pl-20 duration-300">
-                        <div className="pt-10 duration-300">
-                            <img className="w-80 h-auto" src="" alt="" />
-                        </div>
-
-                        <div className="flex flex-row pl-23 justify-start gap-3 pt-20 pb-24 items-center">
-                            <a className="text-black text-5xl font-light hover:text-blue-600" href="" onClick="showSignUp()">Sign Up</a>
-                            <p className="text-black text-3xl ">or</p>
-                            <a className="text-black text-5xl font-light hover:text-blue-600" href="" onClick="showSignIn()">Sign In</a>
-                        </div>
-
-                        <div className="flex flex-col justify-start gap-8">
-                            <div>
-                                <h3 className="font-bold text-black pb-2 ">E-MAIL</h3>
-                                <InputLogin variant="login" placeholder="Enter your e-mail" />
-                            </div>
-
-                        </div>
-
-                    </div>
+  return (
+    <>
+      <body className="bg-white">
 
 
-
-                </section>
-
+        <section className="grid grid-cols-2">
 
 
+        <section id="sign-in" className="visible">
+          <div className="grid pl-20 duration-300">
+            <div className="pt-10 duration-300">
+              <img
+                className="w-80 h-auto"
+                src="./src/imgs/Logo_white.png"
+                alt=""
+              />
+            </div>
+
+            <div className="flex flex-row pl-23 justify-start gap-3 pt-20 pb-24 items-center">
+              <a
+                className="text-black text-5xl font-light hover:text-blue-600"
+                href=""
+                onClick={showSignUp}
+              >
+                Sign Up
+              </a>
+              <p className="text-black text-3xl ">or</p>
+              <a
+                className="text-black text-5xl font-light hover:text-blue-600"
+                href=""
+                onClick={showSignIn}
+              >
+                Sign In
+              </a>
+            </div>
+
+                
+            <div className="gap-5 flex flex-col">
+              <InputLogin title={"E-MAIL"} placeholder={"Enter your e-mail"} />
+              <InputLogin
+                title={"PASSWORD"}
+                placeholder={"Enter your password"}
+              />
+            </div>
+            
+
+            <div class="flex flex-row gap-7 items-center pt-5">
+              <Button variant="solid-discord-blue">Sign In</Button>
+              <a class="text-black hover:underline hover:text-blue-500" href="">
+                Did you forget your password?
+              </a>
+            </div>
+          </div>
+        </section>
+
+
+
+        <section className="w-[100%]">
+            <img className="h-screen" src="./src/imgs/background_sign.png" alt="background_signIn" />
+        </section>
 
 
 
 
+        </section>
+        
+       
 
-            </body>
 
-
-
-        </>
-    );
+      </body>
+    </>
+  );
 }
--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,22 @@ export function getCurrentDate(separator = '-') {
 
     return `${date}${separator}${month < 10 ? `0${month}` : `${month}`}${separator}${year}`
 }
+
+
+ export function showSignUp() {
+    document.getElementById('sign-in').classList.add('hidden');
+    document.getElementById('sign-up').classList.remove('hidden');
+    document.getElementById('sign-up-link').classList.add('active');
+    document.getElementById('sign-in-link').classList.remove('active');
+}
+
+export function showSignIn() {
+    document.getElementById('sign-in').classList.remove('hidden');
+    document.getElementById('sign-up').classList.add('hidden');
+    document.getElementById('sign-in-link').classList.add('active');
+    document.getElementById('sign-up-link').classList.remove('active');
+}
+
+export function showSignInFromSignUp() {
+    showSignIn();
+}
