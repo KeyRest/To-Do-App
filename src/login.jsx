f7018486b0e617575edfd1c70249247a21c855a9
IsaacCorellaA <143538498+IsaacCorellaA@users.noreply.github.com>
2024-05-24 21:16:25 -0600

    Responsive finished

--- src/login.jsx
+++ src/login.jsx
@@ -8,30 +8,31 @@ export default function Login() {
       <body className="bg-white">
 
 
-        <section className="grid grid-cols-2">
+        <section className="grid grid-cols-1 md:grid-cols-2">
 
 
         <section id="sign-in" className="visible">
-          <div className="grid pl-20 duration-300">
-            <div className="pt-10 duration-300">
+          <div className="grid md:pl-20 duration-300">
+            <div className="pt-10 duration-300 items-center md:items-start flex justify-center md:justify-start">
               <img
-                className="w-80 h-auto"
+                className="w-80 h-auto hidden md:block "
                 src="./src/imgs/Logo_white.png"
                 alt=""
               />
+              <img className="w-16 h-auto md:hidden " src="./src/imgs/logo_responsive_negro.png" alt="" />
             </div>
 
-            <div className="flex flex-row pl-23 md:justify-start gap-3 pt-20 pb-24 items-center">
+            <div className="flex flex-row pl-23 md:justify-start justify-center gap-3 pt-20 pb-24 items-center">
               <a
-                className="text-black lg:text-5xl font-light hover:text-blue-600"
+                className="text-black lg:text-5xl text-4xl  font-light hover:text-blue-600"
                 href=""
                 onClick={showSignUp}
               >
                 Sign Up
               </a>
-              <p className="text-black lg:text-3xl ">or</p>
+              <p className="text-black lg:text-3xl text-2xl  ">or</p>
               <a
-                className="text-discord-blue lg:text-5xl font-light hover:text-blue-600"
+                className="text-discord-blue lg:text-5xl text-4xl  font-light hover:text-blue-600"
                 href=""
                 onClick={showSignIn}
               >
@@ -40,7 +41,7 @@ export default function Login() {
             </div>
 
                 
-            <div className="gap-5 flex flex-col">
+            <div className="gap-5 flex flex-col justify-center items-center md:items-start">
               <InputLogin title={"E-MAIL"} placeholder={"Enter your e-mail"} />
               <InputLogin
                 title={"PASSWORD"}
@@ -49,7 +50,7 @@ export default function Login() {
             </div>
             
 
-            <div class="flex flex-row gap-7 items-center pt-5">
+            <div class="flex md:flex-row flex-col gap-7 items-center pt-5">
               <Button variant="solid-discord-blue">Sign In</Button>
               <a class="text-black hover:underline hover:text-blue-500" href="">
                 Did you forget your password?
@@ -60,26 +61,27 @@ export default function Login() {
         </section>
 
         <section id="sign-up" className="hidden">
-          <div className="grid pl-20 duration-300">
-            <div className="pt-10 duration-300">
+          <div className="grid md:pl-20 duration-300 ">
+            <div className="pt-10 duration-300 items-center md:items-start flex justify-center md:justify-start">
               <img
-                className="w-80 h-auto"
+                className="w-80 h-auto hidden md:block"
                 src="./src/imgs/Logo_white.png"
                 alt=""
               />
+               <img className="w-16 h-auto md:hidden " src="./src/imgs/logo_responsive_negro.png" alt="" />
             </div>
 
-            <div className="flex flex-row pl-23 md:justify-start gap-3 pt-20 pb-24">
+            <div className="flex flex-row pl-23 md:justify-start justify-center gap-3 pt-20 pb-24">
               <a
-                className="text-black lg:text-5xl font-light hover:text-blue-600"
+                className="text-black lg:text-5xl text-4xl font-light hover:text-blue-600"
                 href=""
                 onClick={showSignUp}
               >
                 Sign Up
               </a>
-              <p className="text-black lg:text-3xl ">or</p>
+              <p className="text-black lg:text-3xl text-2xl ">or</p>
               <a
-                className="text-black lg:text-5xl font-light hover:text-blue-600"
+                className="text-black lg:text-5xl text-4xl font-light hover:text-blue-600"
                 href=""
                 onClick={showSignIn}
               >
@@ -88,7 +90,7 @@ export default function Login() {
             </div>
 
                 
-            <div className="gap-5 flex flex-col">
+            <div className="gap-5 flex flex-col justify-center items-center md:items-start">
               <InputLogin title={"FULLNAME"} placeholder={"Enter your fullname"} />
               <InputLogin
                 title={"PASSWORD"}
@@ -101,8 +103,8 @@ export default function Login() {
             </div>
             
 
-            <div class="flex flex-row gap-7 items-center pt-5">
-              <Button variant="solid-discord-blue">Sign In</Button>
+            <div class="flex md:flex-row flex-col gap-7 items-center pt-5">
+              <Button variant="solid-discord-blue">Sign Up</Button>
               <a class="text-black hover:underline hover:text-blue-500" href="">
                 Did you forget your password?
               </a>
